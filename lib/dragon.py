1eb05b4ffe16d5d6c5b1afd746dd4aa48a0a7501
Shaun James <ShaunDaneJames@Gmail.com>
2021-03-15 10:02:53 -0600

    learns about returning out of if statments

--- lib/dragon.py
+++ lib/dragon.py
@@ -1,15 +1,15 @@
 class Dragon:
-    def __init__(self, name, color, rider, hungry = True, eaten = 0):
+    def __init__(self, name, color, rider, hungry = True, satiety = 0):
         self.name = name
         self.color = color
         self.rider = rider
         self.hungry = hungry
-        self.eaten = eaten
+        self.satiety = satiety
 
     def is_hungry(self):
-        if self.eaten > 2:
+        if self.satiety > 2:
             self.hungry = False
         return self.hungry
 
     def eat(self):
-        self.eaten += 1
+        self.satiety += 1
